411e74069e38fe20c1ca31fc915c8a71d6483cec
Dejan Andjelic <pyranth95@hotmail.com>
2021-04-24 17:29:14 +0200

    Movies list and search initial

---
+++ DataAccess/Migrations/20210424152709_MoviesSeed.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class MoviesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "CoverImage", "Description", "Name", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "", "", "The Shawshank Redemption", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "", "", "The Godfather", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "", "", "The Godfather: Part II", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, "", "", "The Dark Knight", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, "", "", "12 Angry Men", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6, "", "", "Schindler's List", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 7, "", "", "The Lord of the Rings: The Return of the King", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 8, "", "", "Pulp Fiction", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 9, "", "", "The Good, the Bad and the Ugly", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 10, "", "", "The Lord of the Rings: The Fellowship of the Ring", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 11, "", "", "Fight Club", new DateTime(1994, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 11);
+        }
+    }
+}
---
+++ MistralMoviesApp/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Infrastructure;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MistralMoviesApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MoviesDbContext _context;
+        public MoviesController(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(int loaded = 0, string text = "")
+        {
+            if (text == null)
+                text = "";
+
+            var list = _context.Movies;
+
+            var result = list.Where(m => m.Name.Contains(text)).Skip(loaded).Take(10);
+
+            loaded += result.Count();
+
+            ViewBag.Loaded = loaded;
+
+            return PartialView(result);
+        }
+    }
+}
